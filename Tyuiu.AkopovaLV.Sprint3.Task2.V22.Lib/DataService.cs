5728775c12bc47ca5de7314958030167072f09b4
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-25 08:04:22 -1200

    Task3

---
+++ Tyuiu.AkopovaLV.Sprint3.Task2.V22.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AkopovaLV.Sprint3.Task2.V22.Lib
+{
+    public class DataService : ISprint3Task2V22
+    {
+        public double GetMultiplySeries(int value, int startValue, int stopValue)
+        {
+            double proz = 1;
+            do
+            {
+                proz = proz * Math.Pow(3 / (startValue + Math.Pow(value, -startValue)), 2);
+                startValue++;
+            } while (startValue <= stopValue);
+            return Math.Round(proz, 3);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint3.Task2.V22.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.AkopovaLV.Sprint3.Task2.V22.Lib;
+namespace Tyuiu.AkopovaLV.Sprint3.Task2.V22.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int value = 5;
+            int stValue = 1;
+            int enValue = 7;
+            var res = ds.GetMultiplySeries(value, stValue, enValue);
+            double wait = 0.125;
+            Assert.AreEqual(wait, res);
+
+        }
+    }
+}
