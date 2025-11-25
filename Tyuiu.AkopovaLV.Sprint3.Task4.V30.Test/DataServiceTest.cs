2a6512aff1373e52d7f323f612d19d0cc66dfde4
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-25 09:50:09 -1200

    Task4

---
+++ Tyuiu.AkopovaLV.Sprint3.Task4.V30.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AkopovaLV.Sprint3.Task4.V30.Lib;
+namespace Tyuiu.AkopovaLV.Sprint3.Task4.V30.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int stValue = -5;
+            int nwValue = 5;
+            var res = ds.Calculate(stValue, nwValue);
+            double wait = 1.924;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
