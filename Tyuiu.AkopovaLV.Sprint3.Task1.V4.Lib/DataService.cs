58575f13c90dd24aa45137f5bb51b5e45f3225a3
AkopovaLuda <ludmilaakopova27@gmail.com>
2025-11-25 07:09:18 -1200

    Task1

---
+++ Tyuiu.AkopovaLV.Sprint3.Task1.V4.Lib/DataService.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using tyuiu.cources.programming.interfaces.Sprint3;
+namespace Tyuiu.AkopovaLV.Sprint3.Task1.V4.Lib
+{
+    public class DataService : ISprint3Task1V4
+    {
+        public double GetSumSeries(int startValue, int stopValue)
+        {
+            double sum = 0;
+            while (startValue <= stopValue)
+            {
+                sum = sum + 1 /(Math.Pow( startValue, 2));
+                startValue++;
+            }
+            return Math.Round(sum, 3);
+        }
+    }
+}
---
+++ Tyuiu.AkopovaLV.Sprint3.Task1.V4.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.AkopovaLV.Sprint3.Task1.V4.Lib;
+namespace Tyuiu.AkopovaLV.Sprint3.Task1.V4.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+            double res = ds.GetSumSeries(startValue, stopValue);
+            double wait = 1.55;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
